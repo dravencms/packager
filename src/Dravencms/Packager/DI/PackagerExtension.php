c59b904427cdf6baa1d3fcb91cbf7221fd6133bf
Adam Schubert <adam.schubert@sg1-game.net>
2016-11-30 06:50:18 +0100

    Remove useless use

--- src/Dravencms/Packager/DI/PackagerExtension.php
+++ src/Dravencms/Packager/DI/PackagerExtension.php
@@ -6,7 +6,6 @@ use Kdyby\Console\DI\ConsoleExtension;
 use Nette;
 use Nette\DI\Compiler;
 use Nette\DI\Configurator;
-use Salamek\Cms\DI\CmsExtension;
 /**
  * Class Packagerxtension
  * @package Dravencms\Packager\DI
